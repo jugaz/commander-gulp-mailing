58295109a5a53d68916148b8805b5cf5fd9ca3ff
Jesús Antonio Ugaz Vegas <jesusugazv@gmail.com>
2020-12-12 23:54:35 -0500

    fix:correción de bugs

--- bin/mailing.js
+++ bin/mailing.js
@@ -3,21 +3,14 @@
 'use strict';
 
 const
-    autoprefixer = require('autoprefixer'),
     debug = require('gulp-debug'),
     inlineCSS = require('gulp-inline-css'),
     path = require('path'),
+    gulpPugBeautify = require('gulp-pug-beautify'),
     program = require('commander'),
     pug = require('gulp-pug'),
     util = require('gulp-util'),
     { src, dest, series, parallel } = require("gulp");
-
-
-var Plugins = [
-    autoprefixer({
-        overrideBrowserslist: ['last 20 version']
-    })
-];
 
 
 /* ######################## OPTIONS ######################## */
@@ -68,6 +61,47 @@ program
             })
             
             .pipe(inlineCSS())
+            .pipe(dest(output))
+            .on('end', function () {
+                util.log('Done!');
+            });;
+        
+
+    })
+program
+    .command('prod:mailing <input>')
+    .option("--m [options]")
+    .action((input, options) => {
+        var input = options.input || options.parent.rawArgs;
+        var output = options.output || options.m;
+
+       
+        
+        input = input.filter(function (index, value) {
+            if (path.extname(index) == ".pug") {
+                return index;
+            }
+
+        });
+        return src(input, { allowEmpty: true })
+            .pipe(debug({
+                title: 'commander-gulp-mailing: production'
+            }))
+            .pipe(pug())
+            .on('error', function (error) {
+                // tenemos un error 
+                util.log("Error Name:", error.name);
+                util.log("Error Code:", error.code);
+                util.log("Error Filename:", error.filename);
+                util.log("Error Line:", error.line);
+                util.log("Error Column:", error.column);
+                util.log("Error Msg", error.Msg);
+
+
+            })
+           
+            .pipe(inlineCSS())
+            .pipe(gulpPugBeautify())
             .pipe(dest(output))
             .on('end', function () {
                 util.log('Done!');
